26e94780382c70a62a527ee5d2dafb91c8c2a5f9
rshekh1 <rshekhar@walmartlabs.com>
2019-09-29 16:12:58 -0700

    More programs

--- misc/src/main/scala/com/raj/Client.scala
+++ misc/src/main/scala/com/raj/Client.scala
@@ -8,6 +8,10 @@ object Client {
 
   def main(args: Array[String]): Unit = {
     println(s"Running main: output = ${getItems}")
+    val map = Map("k1" -> "v1", "k2" -> "v2")
+    val m = map + ("k3" -> "v3")
+    println(map)
+    println(m)
   }
 
 }
