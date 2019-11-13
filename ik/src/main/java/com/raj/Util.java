815eab676db3cb1e614fc76f9762648c56d18d69
rshekh1 <rshekhar@walmartlabs.com>
2019-11-12 22:28:33 -0800

    More programs

--- ik/src/main/java/com/raj/Util.java
+++ ik/src/main/java/com/raj/Util.java
@@ -30,7 +30,7 @@ public class Util {
     }
 
     public static boolean isPalin(String s) {
-        if (s == null || s.isEmpty()) return false;
+        if (s == null || s.isEmpty()) return true;
         if (s.length() == 1) return true;
         for (int i = 0, j=s.length()-1; i < j; i++,j--) {
             if (s.charAt(i) != s.charAt(j)) return false;
--- ik/src/main/java/com/raj/strings/PalindromePairs.java
+++ ik/src/main/java/com/raj/strings/PalindromePairs.java
@@ -19,7 +19,8 @@ public class PalindromePairs {
     }
 
     /**
-     * BruteForce: O(n^2). 2 loops. Fix a word, combine it with other string in list, check each combined string is palindrome or not
+     * BruteForce: O(n^2) * L, where L = max word length.
+     *  => 2 loops. Fix a word, combine it with other string in list, check each combined string is palindrome or not
      *
      * Trie?
      *               --- root ---
@@ -45,51 +46,45 @@ public class PalindromePairs {
      *                a
      *                r
      *      -> Some dupes like "doggod" & "goddog" will be found. Use Set to avoid dupes.
-     * Complexity - Runtime is O(n) at the the cost O(n) of building Trie, where n = number of words
+     * Complexity - Runtime is O(n * L) at the the cost O(n * L) of building Trie, where n = number of words
      */
     static List<String> findPalinPairs(List<String> words) {
         Set<String> res = new HashSet<>();
         List<String> reverseWords = words.stream().map(x -> reverseStr(x)).collect(Collectors.toList());
 
         // search reverse word in regular Trie
-        Trie trie = buildTrie(words);
-        for (String w : reverseWords) {
-            int idx = isPrefixTrie(trie.root, w);
-            if (idx > 0) {  // partial or full match
-                if (idx == w.length()) {
-                    res.add(w.substring(0, idx) + reverseStr(w));  // FIX ME, doesn't yield correct results
-                }
-                else {
-                    if (Util.isPalin(w.substring(idx))) {
-                        res.add(w.substring(0, idx) + reverseStr(w));
-                    }
-                }
-            }
-        }
+        Trie regularTrie = buildTrie(words);
+        searchTrie(reverseWords, regularTrie, res);
 
         // search regular word in reverse Trie
-        trie = buildTrie(reverseWords);
-        for (String w : words) {
-            int idx = isPrefixTrie(trie.root, w);
-            if (idx > 0) {  // partial or full match
-                if (idx == w.length()) res.add(w.substring(0, idx) + w);
-                else {
-                    if (Util.isPalin(w.substring(idx))) res.add(w.substring(0, idx) + w);
-                }
-            }
-        }
+        Trie reverseTrie = buildTrie(reverseWords);
+        searchTrie(words, reverseTrie, res);
 
         return new ArrayList<>(res);
     }
 
+    static void searchTrie(List<String> words, Trie trie, Set<String> res) {
+        for (String w : words) {
+            int matchedIdx = isPrefixWordInTrie(trie.root, w);
+            if (matchedIdx > 0) {  // partial or full match
+                if (Util.isPalin(w.substring(matchedIdx))) {
+                    res.add(w + reverseStr(w.substring(0, matchedIdx)));
+                }
+            }
+        }
+    }
+
     // returns the index until it matches in trie
-    static int isPrefixTrie(TNode node, String word) {
-        int i = 0;
+    static int isPrefixWordInTrie(TNode node, String word) {
+        int i;
         for (i = 0; i < word.length(); i++) {
-            if (!node.childs.containsKey(word.charAt(i))) return i;
+            if (!node.childs.containsKey(word.charAt(i))) {
+                if (node.childs.containsKey('$')) return i; // only return if prefix is word in trie
+                else return 0;
+            }
             node = node.childs.get(word.charAt(i));
         }
-        return i;
+        return node.childs.containsKey('$') ? i : 0;
     }
 
 }
